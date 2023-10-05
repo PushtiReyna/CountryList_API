c43170d82f0bc5f9176821f82c4a995a5e8e072e
Pushti Prajapati <pushti.prajapati@reynasolutions.com>
2023-10-05 10:08:53 +0530

    PP- API CALL

---
+++ CountryList/Controllers/CountryController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Data;
+using System.Net;
+
+namespace CountryList.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CountryController : ControllerBase
+    {
+        public async Task<ActionResult> GetCountry()
+        {
+
+            DataTable dt = new DataTable();
+            using(var client  = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://restcountries.com/");
+                using (HttpResponseMessage response = await client.GetAsync("/v3.1/all/"))
+                {
+                    var responseContent = response.Content.ReadAsStringAsync().Result;
+                    dt = (DataTable)JsonConvert.DeserializeObject(responseContent, typeof(DataTable));
+                    response.EnsureSuccessStatusCode();
+
+                    return Ok(dt);
+                }
+            }
+           
+        }
+    }
+}
+
---
+++ CountryList/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using CountryList.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Net;
+
+namespace CountryList.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult Countrylist()
+        {
+            return View();
+        }
+        public ActionResult GetCountry()
+        {
+            string strurltest = string.Format("https://restcountries.com/v3.1/all");
+            WebRequest requestObjGet = WebRequest.Create(strurltest);
+            requestObjGet.Method = "GET";
+            HttpWebResponse responseObjGet = null;
+            responseObjGet = (HttpWebResponse)requestObjGet.GetResponse();
+
+            string strresulttest = null;
+           
+            using (Stream stream = responseObjGet.GetResponseStream())
+            {
+                StreamReader sr = new StreamReader(stream);
+                strresulttest = sr.ReadToEnd();
+               
+                sr.Close();
+                return View(strresulttest);
+            }
+            
+        }
+        
+
+    }
+}
